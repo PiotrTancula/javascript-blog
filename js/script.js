6c93b62f2bc2d07815bdc173139a55b0ec64f429
Piotr Tańcula <tanculapiotr@gmail.com>
2022-05-11 16:17:40 +0200

    clicked articles displayed in the center feature

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 //   });
 
 const titleClickHandler = function (event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -31,9 +32,18 @@ const titleClickHandler = function (event) {
 
     /* get 'href' attribute from the clicked link */
 
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+
     /* find the correct article using the selector (value of 'href' attribute) */
 
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
     /* add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+
   }
 
   const links = document.querySelectorAll('.titles a');
